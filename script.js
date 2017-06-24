7a32d9117353b758603eb8a6041471a841a4623b
Edgar Juarez <ejjt.686@gmail.com>
2017-06-24 13:35:28 -0700

    Class of active is added to slider when clicked and hold

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ let scrollLeft;
 
 slider.addEventListener('mousedown',() => {
     isDown = true;
+    slider.classList.add('active');
 });
 slider.addEventListener('mouseleave',() => {
     isDown = false;
+    slider.classList.remove('active');
 
 });
 slider.addEventListener('mouseup',() => {
     isDown = false;
+    slider.classList.remove('active');
 
 });
 slider.addEventListener('mousemove',() => {
